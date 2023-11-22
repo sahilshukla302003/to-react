f80d4d7ab19a097df2f43002405e791bf0683197
sahilshukla302003 <sahilshukla302003@gmail.com>
2023-11-22 12:03:21 +0530

    u

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import { CalendarDate, CaretUp } from 'react-bootstrap-icons'
 function Calendar(){
 
     return (
         <div className='Calendar'>
-            Calendar
+            <div className='header' >
+                <div className='title'>
+                    <CalendarDate size="18"/>
+                    <p>Calendar</p>
+                    
+                    </div>
+               <div className='btns' >
+                 <span>
+                        <CaretUp size="20"/>
+
+                 </span>
+
+               </div>
+
+            </div>
+           
         </div>
     )
 }
